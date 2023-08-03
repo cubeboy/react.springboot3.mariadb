71218e4aedf82e88acc1d453af2c0e2771e017eb
cubeboy <cubeboy.kim@gmail.com>
2023-08-03 17:49:25 +0900

    implement authentication exception

--- cardatabase/src/main/java/com/jinnara/cardatabase/config/SecurityConfig.java
+++ cardatabase/src/main/java/com/jinnara/cardatabase/config/SecurityConfig.java
@@ -1,41 +1,63 @@
 package com.jinnara.cardatabase.config;
 
 import com.jinnara.cardatabase.service.JwtFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
+  private final JwtFilter jwtFilter;
+
+  private final AuthEntryPoint exceptionHandler;
+
+  public SecurityConfig(JwtFilter jwtFilter, AuthEntryPoint exceptionHandler) {
+    this.jwtFilter = jwtFilter;
+    this.exceptionHandler = exceptionHandler;
+  }
+
   @Bean
   SecurityFilterChain webFilterChain(HttpSecurity http) throws Exception {
     return http
         .csrf(AbstractHttpConfigurer::disable)
-        .cors(AbstractHttpConfigurer::disable)
+        .cors(it -> it.configurationSource(corsConfigurationSource()))
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests(it -> {
-          it
-              .requestMatchers(HttpMethod.POST, "/login").permitAll()
-              .anyRequest().authenticated();
-        })
+        .authorizeHttpRequests(it -> it
+            .requestMatchers(HttpMethod.POST, "/login").permitAll()
+            .anyRequest().authenticated())
+        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+        .exceptionHandling(it -> it.authenticationEntryPoint(this.exceptionHandler))
         .build();
   }
 
   @Bean
   public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
     return auth.getAuthenticationManager();
+  }
+
+  private CorsConfigurationSource corsConfigurationSource() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(List.of("*"));
+    config.setAllowedMethods(List.of("*"));
+    config.setAllowedHeaders(List.of("*"));
+    config.setAllowCredentials(false);
+    config.applyPermitDefaultValues();
+    source.registerCorsConfiguration("/**", config);
+    return source;
   }
 
   @Bean PasswordEncoder passwordEncoder() {
--- cardatabase/src/main/java/com/jinnara/cardatabase/service/JwtFilter.java
+++ cardatabase/src/main/java/com/jinnara/cardatabase/service/JwtFilter.java
@@ -2,17 +2,39 @@ package com.jinnara.cardatabase.service;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
-public class JwtFilter extends GenericFilterBean {
+public class JwtFilter extends OncePerRequestFilter {
+  private final JwtService jwtService;
+
+  public JwtFilter(JwtService jwtService) {
+    this.jwtService = jwtService;
+  }
+
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-    chain.doFilter(request, response);
+  protected void doFilterInternal(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+    String jwt = request.getHeader(HttpHeaders.AUTHORIZATION);
+    if (jwt != null) {
+      String user = jwtService.getAuthUser(request);
+      Authentication authentication =
+          new UsernamePasswordAuthenticationToken(user, null, Collections.emptyList());
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    filterChain.doFilter(request, response);
   }
 }
